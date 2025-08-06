19b203a383f65360c86655c2c7ea1afce736a1c0
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-06 11:40:58 +0100

    first commit in a while

---
+++ src/main/java/dev/engripaye/universal_digital_health_record_access_system/UniversalDigitalHealthRecordAccessSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.universal_digital_health_record_access_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalDigitalHealthRecordAccessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalDigitalHealthRecordAccessSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/universal_digital_health_record_access_system/UniversalDigitalHealthRecordAccessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.universal_digital_health_record_access_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalDigitalHealthRecordAccessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
